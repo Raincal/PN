ef00d0f232f5ac653d1e58fdac3ecece2fc7e999
Raincal <cyj94228@gmail.com>
2016-03-23 02:29:52 +0800

    fix navList

--- app/constants/navList.js
+++ app/constants/navList.js
@@ -1,5 +1,7 @@
-export default navList = {
+const navList = {
     category: ['全部', '演唱会', '话剧歌剧', '音乐会', '儿童亲子', '休闲娱乐', '舞蹈芭蕾', '曲苑杂坛'],
     sort: ['最热', '最新'],
     time: ['全部时间', '一周内', '一月内']
 }
+
+export default navList
